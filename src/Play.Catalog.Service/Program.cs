228652b9d15cc23389d04797abed9e3dc062078c
Amrit <AmritMaan2221@gmail.com>
2026-01-04 20:39:19 -0500

    Add Scalar Docs for API

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,16 +1,13 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
 builder.Services.AddControllers(); 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger(); // serves /swagger/v1/swagger.json
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "API v1");
-    });
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
