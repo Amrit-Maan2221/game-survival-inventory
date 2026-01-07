67828fe2db93baee70e25124827dad2e7eca1b73
Amrit <AmritMaan2221@gmail.com>
2026-01-06 23:51:02 -0500

    Implement Circuit Breaker

--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -8,10 +8,10 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
-builder.Services.AddControllers(options=>
+builder.Services.AddControllers(options =>
 {
     options.SuppressAsyncSuffixInActionNames = false;
-}); 
+});
 
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("items");
 Random jitter = new();
@@ -19,9 +19,12 @@ builder.Services.AddHttpClient<CatalogClient>((client) =>
 {
     var catalogServiceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
     client.BaseAddress = new Uri("https://localhost:7009");
-}).AddTransientHttpErrorPolicy(builder => 
-    builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) + (jitter.Next(0,1000) / 1000.0))))
-    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
+}).AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync
+        (3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) + (jitter.Next(0, 1000) / 1000.0)))
+        )
+        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync
+        (5, TimeSpan.FromSeconds(30))
+    ).AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
 
 var app = builder.Build();
 
