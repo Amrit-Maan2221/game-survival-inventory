724957407ebad7cff687d7a30bd90ff4bd0604e6
Amrit <AmritMaan2221@gmail.com>
2026-01-11 11:50:01 -0500

    Add lazy loading

--- src/Play.Frontend/src/App.jsx
+++ src/Play.Frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
-import Home from "./pages/Home";
-import Catalog from "./pages/Catalog";
-import Inventory from "./pages/Inventory";
-import CatalogItem from "./pages/CatalogItem";
+import { lazy, Suspense } from "react";
+const Home = lazy(() => import("./pages/Home"));
+const Catalog = lazy(() => import("./pages/Catalog"));
+const CatalogItem = lazy(() => import("./pages/CatalogItem"));
+const Inventory = lazy(() => import("./pages/Inventory"));
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/catalog/:id" element={<CatalogItem />} />
-        <Route path="/inventory" element={<Inventory />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading page...</div>}>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<CatalogItem />} />
+          <Route path="/inventory" element={<Inventory />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
