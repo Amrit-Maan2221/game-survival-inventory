f930b898c6647df1a8985cff01999bd114a383e7
Amrit <AmritMaan2221@gmail.com>
2026-01-11 00:47:08 -0500

    Fix price for catalog item

--- src/Play.Inventory.Service/Entities/CatalogItem.cs
+++ src/Play.Inventory.Service/Entities/CatalogItem.cs
@@ -11,6 +11,5 @@ public class CatalogItem : IEntity
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
-    public decimal Price { get; set; }
     public DateTimeOffset CreatedDate { get; set; }
 }
